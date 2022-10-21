5400fdd60fca8e1edb2bdf2ce136802a950f3ceb
LanTianTianZhao <115973725+LanTianTianZhao@users.noreply.github.com>
2022-10-21 13:24:48 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4 ");
         System.out.println("hello git hot fix ");
         System.out.println("hello git push test ");
+        System.out.println("hello git pull test ");
     }
 }
