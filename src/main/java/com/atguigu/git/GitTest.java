319c5aee3ed36e57fed2ba2493150dd6c1c381fe
LanTianTianZhao <115973725+LanTianTianZhao@users.noreply.github.com>
2022-10-21 13:30:52 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git hot fix ");
         System.out.println("hello git push test ");
         System.out.println("hello git pull test ");
+        
     }
 }
